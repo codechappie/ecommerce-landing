c8f28624b4d52b1b200199385b948412a777e2b6
codechappie <davidproyecto01@gmail.com>
2019-12-22 19:17:06 -0500

    update v4.1

---
+++ js/main.js
@@ -0,0 +1,37 @@
+window.sr = ScrollReveal();
+sr.reveal('.main__banner .img__model',{
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('.main__banner .main__slogan',{
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.our_products__title',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '10px'
+});
+sr.reveal('.grid__products .card',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
+sr.reveal('.left__efect',{
+    duration: 2000,
+    origin: 'left',
+    distance: '200px'
+});
+sr.reveal('.right__efect',{
+    duration: 2000,
+    origin: 'right',
+    distance: '200px'
+});
+sr.reveal('.bottom__efect',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
+
